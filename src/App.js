ecd993216cb8a13ea5e3feb13ff0343966c8e050
JC Grennan <jgrennan94@gmail.com>
2022-08-17 19:32:56 +0100

    installed nanoid and html-entities. plus setup

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react"
+import Start from "./components/Start"
+import Quiz from "./components/Quiz"
+
+export default function App() {
+    
+    const [quiz, setQuiz] = React.useState({})
+    const [difficulty, setDifficulty] = React.useState("medium")
+    const [quizGenerated, setQuizGenerated] = React.useState(false)
+    
+    function handleDifficulty(event) {
+            setDifficulty(event.target.value)
+    }
+    
+    function handleQuiz() {
+        setQuizGenerated(prevState => !prevState)
+    }
+    
+    React.useEffect(()=> {
+        fetch(`https://opentdb.com/api.php?amount=5&difficulty=${difficulty}&type=multiple`)
+            .then(res => res.json())
+            .then(data => setQuiz(data))
+    },[difficulty])
+    
+    return(
+        <div>
+            {!quizGenerated && <Start
+                difficulty={difficulty}
+                handleDifficulty={handleDifficulty}
+                handleQuiz={handleQuiz}
+            />}
+            {quizGenerated && <Quiz 
+                quiz={quiz.results}
+                handleDifficulty={handleDifficulty}
+                handleQuiz={handleQuiz}
+            />}
+        </div>
+    )
+}
